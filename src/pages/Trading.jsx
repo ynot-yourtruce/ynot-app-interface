5762a4a434a4a1009cd7a0d419973895751c7b99
bornbig <72680443+bornbig@users.noreply.github.com>
2021-07-21 00:04:48 +0530

    Added NFT positions

--- src/pages/Trading.jsx
+++ src/pages/Trading.jsx
@@ -16,19 +16,48 @@ class Trading extends Component {
           <div className="headerPadding">
             <Back />
             <div className="marketMaker">
-              <Settings />
+              <div className="sbox">
+                <Settings />
+              </div>
 
               <div className="contract-list">
                 {this.props.pair && (
-                  // <div className="row">
-                  //   <div className="col-md-8">
-                  <PoolCard />
-                  //   </div>
-                  //   <div className="col-md-4">
-                  //     <h3>Open Positions</h3>
-                  //   </div>
-                  // </div>
+                  <div className="row">
+                    <PoolCard />
+                    <h2 className="mt-4 mb-4">Open Positions</h2>
+                    <div className="row">
+                      <div className="col-3">
+                        <div className="nft swabox">
+                          <div className="img">
+                            <img
+                              src="https://cdn.coinranking.com/nft/0x60F80121C31A0d46B5279700f9DF786054aa5eE5/30817.png?size=autox430"
+                              alt=""
+                            />
+                          </div>
+                          <div className="amount">100 USDT</div>
+                          <div className="pnl green">2.3%</div>
+                          <div className="date">2 Days Ago</div>
+                          <div className="btn block btn-small mt-2">Close</div>
+                        </div>
+                      </div>
+                      <div className="col-3">
+                        <div className="nft swabox">
+                          <div className="img">
+                            <img
+                              src="https://cdn.coinranking.com/nft/0x60F80121C31A0d46B5279700f9DF786054aa5eE5/30817.png?size=autox430"
+                              alt=""
+                            />
+                          </div>
+                          <div className="amount">100 USDT</div>
+                          <div className="pnl green">2.3%</div>
+                          <div className="date">2 Days Ago</div>
+                          <div className="btn block btn-small mt-2">Close</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 )}
+                <br />
               </div>
             </div>
           </div>
